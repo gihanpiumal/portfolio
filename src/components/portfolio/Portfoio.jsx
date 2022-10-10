e5c2edac709551ef74db4cb51c5657981b7980a4
gihanpiumal <gihanpiumal12345@gmail.com>
2022-10-10 21:37:27 +0530

    add first project

--- src/components/portfolio/Portfoio.jsx
+++ src/components/portfolio/Portfoio.jsx
@@ -6,21 +6,22 @@ import Project from "./project/Project";
 
 import "./portfolio.scss";
 
+
 const Portfoio = () => {
   const temp_project = [
     {
       title: "Memory Blog App",
-      img: "https://www.youtube.com/embed/fH23SP7tZFo",
+      img: "https://www.youtube.com/embed/BhoUIpkolU4",
       subTitle: "Save my memories to my memoty blog",
-      description:"description 1",
-      gitLink: "https://github.com/gihanpiumal/memory-application-v1"
+      description: "description 1",
+      gitLink: "https://github.com/gihanpiumal/memory-application-v1",
     },
   ];
   return (
     <div className="portfolio" id="projects">
       <div className="wrapper-portfolio">
         <div className="title-portfolio">Projects</div>
-        <div className="body-portfolio" >
+        <div className="body-portfolio">
           <Grid
             className="grid"
             container
